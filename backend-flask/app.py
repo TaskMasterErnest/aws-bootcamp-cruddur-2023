66500e94a00e191575a884b3c36273e5d7d20138
TaskMasterErnest <kluernest08@gmail.com>
2023-03-02 17:23:31 +0000

    implemented cloudwatch logs

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -37,6 +37,22 @@ trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
 
+#CLOUDWATCH LOGS -----
+import watchtower
+import logging
+from time import strftime
+
+# Configuring Logger to Use CloudWatch
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.DEBUG)
+console_handler = logging.StreamHandler()
+cw_handler = watchtower.CloudWatchLogHandler(log_group='cruddur')
+LOGGER.addHandler(console_handler)
+LOGGER.addHandler(cw_handler)
+LOGGER.info("Test Log")
+
+
+
 app = Flask(__name__)
 # AWS X-RAY ----------
 XRayMiddleware(app, xray_recorder)
@@ -54,6 +70,13 @@ cors = CORS(
   allow_headers="content-type,if-modified-since",
   methods="OPTIONS,GET,HEAD,POST"
 )
+
+#some cloudwatch_logs stuff
+@app.after_request
+def after_request(response):
+    timestamp = strftime('[%Y-%b-%d %H:%M]')
+    LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
+    return response
 
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
@@ -90,9 +113,10 @@ def data_create_message():
     return model['data'], 200
   return
 
+#added some config for cloudwatch-logs
 @app.route("/api/activities/home", methods=['GET'])
 def data_home():
-  data = HomeActivities.run()
+  data = HomeActivities.run(logger=LOGGER)
   return data, 200
 
 @app.route("/api/activities/notifications", methods=['GET'])
