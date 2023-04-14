35885f0d7652519d2b5725ba8d375477198e2da8
TaskMasterErnest <kluernest08@gmail.com>
2023-04-14 04:01:33 +0000

    configured messaging between users

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,14 +1,13 @@
 -- this file was manually created (because we might auto generate it in future)
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
-  ('Andrew Brown', 'andrew@exampro.co', 'andrewbrown' ,'MOCK'),
   ('Ernest', 'kluernest08@gmail.com', 'Ernest', 'MOCK'),
-  ('Andrew Bayko', 'bayko@exampro.co', 'bayko' ,'MOCK');
+  ('Taskmaster', 'woeliernest@gmail.com', 'taskmaster' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
-    (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
+    (SELECT uuid from public.users WHERE users.handle = 'Ernest' LIMIT 1),
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
   )
