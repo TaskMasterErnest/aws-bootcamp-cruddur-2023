b6caf90cae62fc4941cb4e137f88fa34c45673ea
TaskMasterErnest <kluernest08@gmail.com>
2023-07-13 15:42:00 +0000

    updated the seed.sql  syntax

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,8 +1,8 @@
 -- this file was manually created (because we might auto generate it in future)
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
-  ('The Taskmaster', 'kluernest08@gmail.com', 'thetaskmaster' ,'MOCK');
-  ('Londo Mollari','lmollari@centari.com' ,'londo' ,'MOCK');
+  ('The Taskmaster', 'kluernest08@gmail.com', 'thetaskmaster' ,'9f1f224b-a215-45e6-8476-206965f90261'),
+  ('Londo Mollari','lmollari@centari.com' ,'londo' ,'MOCK'),
   ('Alt Taskmaster', 'woeliernest@gmail.com', 'alt_taskmaster' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
