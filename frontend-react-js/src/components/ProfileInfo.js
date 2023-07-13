30a040c07126a47be3c8171a1d90f1c76c43767d
TaskMasterErnest <kluernest08@gmail.com>
2023-07-13 17:14:05 +0000

    troubleshooting the application, got part of Profile to show, replies not working yet.

--- frontend-react-js/src/components/ProfileInfo.js
+++ frontend-react-js/src/components/ProfileInfo.js
@@ -3,7 +3,7 @@ import {ReactComponent as ElipsesIcon} from './svg/elipses.svg';
 import ProfileAvatar from 'components/ProfileAvatar'
 import React from "react";
 
-// [TODO] Authenication
+// [TODO] Authentication
 import { Auth } from 'aws-amplify';
 
 export default function ProfileInfo(props) {
