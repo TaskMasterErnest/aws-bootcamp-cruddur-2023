df929be01d09091dccd12da30cee98cdc235e7d0
TaskMasterErnest <kluernest08@gmail.com>
2023-07-14 09:17:29 +0000

    reverted to the old script

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,7 +1,7 @@
 -- this file was manually created (because we might auto generate it in future)
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
-  ('The Taskmaster', 'kluernest08@gmail.com', 'thetaskmaster' ,'9f1f224b-a215-45e6-8476-206965f90261'),
+  ('The Taskmaster', 'kluernest08@gmail.com', 'thetaskmaster' ,'MOCK'),
   ('Londo Mollari','lmollari@centari.com' ,'londo' ,'MOCK'),
   ('Alt Taskmaster', 'woeliernest@gmail.com', 'altmaster' ,'MOCK');
 
