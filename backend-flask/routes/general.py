912a96b157850ff3274b8dbeb9366adbbfd0a6de
TaskMasterErnest <kluernest08@gmail.com>
2023-07-14 05:28:27 +0000

    made syntax changes to these files for better view

--- backend-flask/routes/general.py
+++ backend-flask/routes/general.py
@@ -3,9 +3,9 @@ from flask import request, g
 def load(app):
   @app.route('/api/health-check')
   def health_check():
-    return {'success': True, 'ver': 1}, 200
+    return {'success': True, 'ver': 3}, 200
 
-  @app.route('/rollbar/test')
-  def rollbar_test():
-    g.rollbar.report_message('Hello World!', 'warning')
-    return "Hello World!"
+  #@app.route('/rollbar/test')
+  #def rollbar_test():
+  #  g.rollbar.report_message('Hello World!', 'warning')
+  #  return "Hello World!"
