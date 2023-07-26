ff7d383ddbb194b57f68efb810cf1c7d6d74f197
TaskMasterErnest <kluernest08@gmail.com>
2023-07-26 14:26:01 +0000

    updated index.js to verify JWTs, works perfectly

--- aws/json/lambda/lambda-authorizer/index.js
+++ aws/json/lambda/lambda-authorizer/index.js
@@ -5,32 +5,28 @@ const { CognitoJwtVerifier } = require("aws-jwt-verify");
 const jwtVerifier = CognitoJwtVerifier.create({
   userPoolId: process.env.USER_POOL_ID,
   tokenUse: "access",
-  clientId: process.env.CLIENT_ID//,
+  clientId: process.env.CLIENT_ID, //,
   //customJwtCheck: ({ payload }) => {
   //  assertStringEquals("e-mail", payload["email"], process.env.USER_EMAIL);
   //},
 });
 
 exports.handler = async (event) => {
-  console.log("request:", JSON.stringify(event, undefined, 2));
+  // console.log("request:", JSON.stringify(event, undefined, 2));
 
-  // const jwt = event.headers.authorization;
-  const auth = event.headers.authorization;
-  const jwt = auth.split(" ")[1];
-
+  const jwt = event.headers.authorization.split(" ")[1];
+  let isAuthorized = false;
   try {
     const payload = await jwtVerifier.verify(jwt);
     console.log("Access allowed. JWT payload:", payload);
-    return {
-      isAuthorized: true,
-      "context": {
-          "sub": payload.sub
-      },
-    };    
+    isAuthorized = true;
   } catch (err) {
     console.error("Access forbidden:", err);
-    return {
-      isAuthorized: false,
+  } finally {
+    const response = {
+      isAuthorized: isAuthorized,
     };
+    console.log("response", response);
+    return response;
   }
 };
